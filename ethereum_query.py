0f85d552f881bdc98f328a34a67eff7f6e3c92ed
Darkmountz <zxz@seas.upenn.edu>
2022-02-25 07:57:31 +0000

    week7

--- ethereum_query.py
+++ ethereum_query.py
@@ -9,7 +9,7 @@ w3 = Web3(Web3.HTTPProvider('http://' + IP_ADDR + ':' + PORT))
 #     This line will mess with our autograders, but might be useful when debugging
 #     print( "Connected to Ethereum node" )
 if w3.isConnected():
-    print("Correct!")
+    print(end='')
 else:
     print("Failed to connect to Ethereum node!")
 
